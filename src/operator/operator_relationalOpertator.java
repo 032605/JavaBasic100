1bcec1afded49948fd7877fd6c8cf5f91cf98e45
032605 <032605@naver.com>
2022-05-02 16:01:49 +0900

    basic 1편 완강

---
+++ src/operator/operator_relationalOpertator.java
@@ -0,0 +1,22 @@
+package operator;
+
+public class operator_relationalOpertator {
+	public static void main(String[] args) {
+		
+		int kor=70, eng=40, math=70;
+		boolean rst, rst2, rst3, rst4;
+		boolean a = false;
+		
+		rst = kor==eng;
+		rst2 = kor!=eng;
+		rst3 = kor>eng; 
+		
+		System.out.println(rst + ", " + rst2 + ", " + rst3);	//false, true, true
+		
+		//==================== LogicalOperator ====================
+		rst4 = kor==eng && kor>eng;	//f && t
+				
+		System.out.println(rst4);	//false
+		System.out.println(!a);		//true
+	}
+}
