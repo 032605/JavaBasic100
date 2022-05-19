0a6acf2d9cb58f203a1f36a99207cb86b34e32fa
032605 <032605@naver.com>
2022-05-19 16:16:04 +0900

    method basic 4types

--- src/function/Method.java
+++ src/function/Method.java
@@ -1,5 +1,7 @@
 package function;
 
+import java.util.Scanner;
+
 public class Method {
 	
 	// 메서드 : 다른 언어에서의 함수와 마찬가지로 어떤 특정한 동작이나 처리를 하도록 만들어진 코드 단위. > 재사용성
@@ -26,14 +28,49 @@ public class Method {
 	
 	}
 	
+	// ============ 3) 반환값 O		받는 인자값 X
+	
+	public static String third() {
+		
+		return "hit"; 
+	
+	}
+	
+	// ============ 4) 반환값 O		받는 인자값 O
+	
+	public static String convertCapital(String str) {
+		
+		String rs = str.toUpperCase();
+		
+		return rs;
+	}
+	
 	public static void main(String[] args) {
 	
 		// main method에 static 선언 > static method만 호출 가능.
+		
+		//[1]
 		showMenu();
 		
+		//[2]
 		int a = 5;
 		int b = 6;
 		plus(a,b);
+		
+		//[3]
+		String third = third();
+		
+		System.out.println(third);
+		
+		//[4]
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("소문자를 입력하세요. ===>");
+		String str = sc.next();
+		
+		String rs = convertCapital(str);
+		System.out.println("입력한 소문자의 대문자는 = " + rs);
+		
 	}
 	
 	
